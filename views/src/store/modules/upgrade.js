3de5f0be7f1d7a65ab58a3ea5ae6aacb7cfe4470
kdk <aliswell86@naver.com>
2018-12-26 17:54:21 +0900

    12.26

---
+++ views/src/store/modules/upgrade.js
@@ -0,0 +1,21 @@
+import {createAction, handleActions} from 'redux-actions';
+import {Map/*, List, fromJS*/} from 'immutable';
+// import {pender} from 'redux-pender';
+// import * as api from 'lib/api';
+
+const SET_UPGRADE_TAX = 'weapon/SET_UPGRADE_TAX';
+
+export const setUpgradeTax = createAction(SET_UPGRADE_TAX);
+
+const initialState = Map({
+  upgradeInfo: Map({
+    tax: '0' //-5% ~ 5%
+  })
+});
+
+export default handleActions({
+  [SET_UPGRADE_TAX]: (state, action) => {
+    const {name, value} = action.payload;
+    return state.setIn(['upgradeInfo', name], value);
+  }
+}, initialState);
